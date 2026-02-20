32506f735bd1b3ee1e735e86239cc5dedde2b81c
AsiyaArab <aashiyaarab39@gmail.com>
2026-02-20 04:11:09 -0500

    My first commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+user = 'Asiya'
+age = 20
+amount = 100.12
+print('ab',end ='')
+print('12',end ='')
+# print(f'Welcome {user},you are {age} years old, your balance is amount ${amount:1.4f}')
+print('***********',end='')
